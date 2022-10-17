164e79cdc230d2ac48781d59ecd9f37b0108029c
7verdy <mailvictorp@gmail.com>
2022-10-17 18:25:11 +0200

    feat(stats-calculator/equipment): continue design, api calls and sets display left to handle.

--- src/components/equipment.svelte
+++ src/components/equipment.svelte
@@ -1,11 +1,31 @@
 <script>
-    const areas = [ 'Maille', 'Pompon', 'Caldemount', 'Violetfair',
+    import { areas, sets } from '../stores/stats.js';
+
+    const areasTmp = [ 'Maille', 'Pompon', 'Caldemount', 'Violetfair',
                     'Aldor', 'Sagacia', 'Regalle' ];
+    let selectedArea = 'Maille';
+    let onChangeSelectedArea = 'Maille';
+
+    const onChange = () => {
+        onChangeSelectedArea = selectedArea;
+    }
 </script>
 <div class="equipment">
-    {#each areas as area}
+    <select id="area-selector" bind:value={selectedArea} on:change={onChange}>
+        {#each areasTmp as area}
+        <option value={area}>{area}</option>
+        {/each}
+    </select>
     <div class="area">
-        {area}
+        {onChangeSelectedArea}
+        {#each sets as set}
+        <div class="set">
+            {set.name}
+            {#each set.items as item}
+            <div class="item">
+                <img src={item.image} alt={item.name} />
+            </div>
+            {/each}
+        </div>
     </div>
-    {/each}
 </div>
--- src/stores/stats.js
+++ src/stores/stats.js
@@ -1,9 +1,29 @@
 const API = 'localhost:7070/api/';
+/**
+ * @type {never[]}
+ */
+export let areas = [];
+/**
+ * @type {never[]}
+ */
+export let sets = [];
+
+export async function getAreas() {
+  const response = await fetch(`${API}/areas`);
+  const data = await response.json();
+  areas = data;
+}
+
+export async function getSets() {
+  const response = await fetch(`${API}/sets`);
+  const data = await response.json();
+  sets = data;
+}
 
 /**
  * @param {object} body
  */
-async function getStats(body) {
+export async function getStats(body) {
   const response = await fetch(`${API}/stats`, {
     method: 'POST',
     headers: {
