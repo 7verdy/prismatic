1ef1b6698539d886b6e976f03bc4b100eb47a84f
Victor-Emmanuel PROVOST <victor-emmanuel.provost@epita.fr>
2022-10-24 21:50:09 +0200

    feat(style): complete basic compendium page.
    Signed-off-by: Victor-Emmanuel PROVOST <victor-emmanuel.provost@epita.fr>

--- src/routes/compendium/+page.svelte
+++ src/routes/compendium/+page.svelte
@@ -6,8 +6,8 @@
 <svelte:head>
     <title>Compendium | Prismatic</title>
 </svelte:head>
-<Banner title="Compendium" />
 <div class="content flex flex-col justify-center items-center">
+<Banner title="Compendium" />
     <input type="search" id="search" placeholder="Enter a query..." bind:value />
     <div class="card results flex justify-center">
         {#if results.length == 0}
