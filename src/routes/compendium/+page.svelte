0b1496b38fc2d29054b481e52526d10495fd1350
Victor-Emmanuel PROVOST <victor-emmanuel.provost@epita.fr>
2023-01-08 15:30:24 +0100

    feat(compendium): add display of data.

--- src/routes/compendium/+page.svelte
+++ src/routes/compendium/+page.svelte
@@ -1,17 +1,52 @@
 <script>
-	import Banner from "../../components/banner.svelte";
-    let value = "";
-    let results = [];
+	import Banner from '../../components/banner.svelte';
+	let value = '';
+	import { gears } from '../../stores/stats.js';
 </script>
+
 <svelte:head>
-    <title>Compendium | Prismatic</title>
+	<title>Compendium | Prismatic</title>
 </svelte:head>
 <div class="content flex flex-col justify-center items-center">
-<Banner title="Compendium" />
-    <input type="search" id="search" placeholder="Enter a query..." bind:value />
-    <div class="card results flex justify-center">
-        {#if results.length == 0}
-            <h2>No result found...</h2>
-        {/if}
-    </div>
+	<Banner title="Compendium" />
+	<input type="search" id="search" placeholder="Enter a query..." bind:value />
+	<div class="card results flex justify-center">
+		{#if $gears.length == 0}
+			<h2>No result found...</h2>
+		{/if}
+		{#each $gears as gear}
+			{#if gear.name.toLowerCase().includes(value.toLowerCase())}
+				<div class="card">
+					<div class="top-row">
+						<h2>{gear.name}</h2>
+						<div class="area-set">
+                            <span>{gear.origin}</span>
+							<span>{gear.set}</span>
+						</div>
+					</div>
+					<p>{gear.description}</p>
+					{#if gear.stats}
+						<h3>Stats</h3>
+						<ul>
+							{#each gear.stats as stat}
+								{#each Object.keys(stat) as key}
+									<li>{key}: {stat[key]}</li>
+								{/each}
+							{/each}
+						</ul>
+					{/if}
+                    {#if gear.hidden}
+                        <h3>Hidden Stats</h3>
+                        <ul>
+                            {#each gear.hidden as stat}
+                                {#each Object.keys(stat) as key}
+                                    <li>{key}: {stat[key]}</li>
+                                {/each}
+                            {/each}
+                        </ul>
+                    {/if}
+				</div>
+			{/if}
+		{/each}
+	</div>
 </div>
--- src/stores/stats.js
+++ src/stores/stats.js
@@ -3,6 +3,7 @@ import { writable } from "svelte/store";
 const API = 'http://127.0.0.1:7070/api';
 
 export const areas = writable([]);
+export const gears = writable([]);
 
 const getAreas = async () => {
   const response = await fetch(`${API}/areas`);
@@ -10,4 +11,11 @@ const getAreas = async () => {
   const areasFetched = data['areas'];
   areas.set(areasFetched);
 };
-getAreas();
+// getAreas();
+
+const getGears = async () => {
+  const response = await fetch(`${API}/gears`);
+  const data = await response.json();
+  gears.set(data);
+}
+getGears();
