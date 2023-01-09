fa4814b8dfec14094aba62403ba274f89cc864f3
Victor-Emmanuel PROVOST <victor-emmanuel.provost@epita.fr>
2023-01-09 23:19:10 +0100

    feat(front): started reworking the design of the website.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,6 @@
 <script>
-    import Nav from "../components/nav.svelte";
     import "../tailwind.css";
     import "../styles.css";
 </script>
-<Nav/>
 <slot></slot>
-<footer>made by <a href="https://github.com/7verdy">verdy</a></footer>
+<!-- <footer>made by <a href="https://github.com/7verdy">verdy</a></footer> -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
 <svelte:head>
-    <title>Index | Prismatic</title>
+	<title>Index | Prismatic</title>
 </svelte:head>
-<div class="content flex flex-row justify-center items-center"></div>
+<div class="content flex items-center justify-center">
+	<div class="welcome flex justify-center items-center flex-col">
+		<h1>Welcome to Prismatic!</h1>
+		<p class="intro">
+			Prismatic is a website aimed at providing Postknight 2 players various tools and information
+			about the game's items to help them in their journey to become the best Postknight of all!
+		</p>
+	</div>
+</div>
